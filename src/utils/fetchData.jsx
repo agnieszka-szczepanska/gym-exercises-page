aaa88de7669496a22280fb54c00cbbc5e4c4fd78
Agnieszka Szczepańska <szczepanska.agnieszka01@gmail.com>
2023-03-08 21:23:12 +0100

    additional fetching configuration

--- src/utils/fetchData.jsx
+++ src/utils/fetchData.jsx
@@ -2,7 +2,7 @@ export const exerciseOptions = {
   method: "GET",
 
   headers: {
-    "X-RapidAPI-Key": "16ff1c23b4msh14376719c058a89p1e550cjsn21b87651dfda", //env dont work -> import.meta.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Key": import.meta.env.VITE_API_KEY,
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
   },
 };
